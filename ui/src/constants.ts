79f10cce09d357e4a35fc02e75265878359367bb
SoimanVasile <vasilecristiansoiman@gmail.com>
2025-12-14 14:06:08 +0200

    .

--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -1,9 +1,9 @@
-export const PACKAGE_ID = "0xd08c200214cf2bdfd0ed7db8c71d1c393909b05c0e83a8f725647771276639d4"; 
+export const PACKAGE_ID = "0xa01d3bb109ef92c6bc3f7afe86ea184a6452db912746ba04eafb0a5141229cca"; 
 export const MODULE_NAME = "ticket_nft";
 export const FUNCTION_NAME = "buy_ticket";
 export const FEATURED_CAMPAIGN_ID ="0xfbaa4c0f52d5ad4ba0ade1bc02a9a60286e30f8e15cc473238355e2c9ab19915";
 
-export const REGISTRY_ID = "0xb8978031ee339a91be189762739165465dc531fde2d166166d264d1bb2322a01";
+export const REGISTRY_ID = "0x06c2bef923e0027afbf56f085c4cf85333b7a85ff7dfc590b35598aa44937a18";
 
 export const ORGANIZER_ADDRESS = "0x100b4d6c7d3e4b8101fd5f4c04036b3130826713100090119619fb2b5bde91ce";
 
